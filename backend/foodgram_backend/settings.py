586892c6c277f9722d11456f373db0d5f39c5cd6
Konstantin Sokolov <dlyakonstantina@gmail.com>
2025-11-29 13:05:55 +0100

    on:

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -26,20 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yrii_bep4g1xl3cq-*jx$_p@n8ots%js5w@-v#5gedik8)rlsq'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-yrii_bep4g1xl3cq-*jx$_p@n8ots%js5w@-v#5gedik8)rlsq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
-ALLOWED_HOSTS = [
-    '127.0.0.1', 'localhost', '51.250.27.166',
-    'foodgramkostya.ddns.net'
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split()
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://foodgramkostya.ddns.net',
-    'https://51.250.27.166'
-]
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost').split()
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
@@ -114,18 +108,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            # 'NAME': BASE_DIR / 'db.sqlite3',
             'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
