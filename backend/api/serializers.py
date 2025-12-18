56f9ed56e8b40a0882e34e6985061e7db0bcfee7
Konstantin Sokolov <dlyakonstantina@gmail.com>
2025-12-18 10:09:21 +0100

    review_4 add_ingredients

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import transaction
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
